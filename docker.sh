f42100a4d14fcd260f07e98e5acf307017e793eb
Sergey <xxsmotur@gmail.com>
2020-06-07 18:07:59 +0500

    Добавление комментариев

--- docker.sh
+++ docker.sh
@@ -1,15 +1,27 @@
-docker images --format "{{.ID}}\t{{.Repository}}\t{{.Size}}" | awk '{sub(/GB/, "", $3)}1' | awk '{$3=($3 ~ /MB/) ? $3 : $3*1000"MB"}1' | sort -k 3 -h
 
+# docker images отсортированный по размеру образа
+dimages_sort(){
+    docker images --format "{{.ID}}\t{{.Repository}}\t{{.Size}}" | awk '{sub(/GB/, "", $3)}1' | awk '{$3=($3 ~ /MB/) ? $3 : $3*1000"MB"}1' | sort -k 3 -h
+}
+
+# вход в контейнер по его имени
+# dexec redis
 dexec() {
     docker exec -ti $(docker ps --filter "name=$1" | awk 'NR>1{print $1}') sh
 }
 
+# Показ списка Volumes по имени драйвера
+# dvolumes local
 dvolumes() {
-    docker volume ls --filter "driver=local" --format '{{.Driver}} {{.Name}} {{.Labels}}' | less
+    docker volume ls --filter "driver=$1" --format '{{.Driver}} {{.Name}} {{.Labels}}' | less
 }
 
-dlogs () {
-    date_old=$(date --rfc-3339=seconds -d "$1"| sed 's/ /T/' | cut -f1 -d"+")
+# Возвращает логи из контейнера по date range
+# date range использует формат команды date
+# dlogs_out redis "-1 days"
+# возвращает логи, которые были в контейнеры со вчерашнего дня до текущего момента
+dlogs_out () {
+    date_old=$(date --rfc-3339=seconds -d "$2"| sed 's/ /T/' | cut -f1 -d"+")
     date_now=$(date --rfc-3339=seconds | sed 's/ /T/' | cut -f1 -d"+")
-    docker logs 050e94357f7d --details --since=$(echo $date_old) --until=$(echo $date_now)
+    docker logs $(docker ps --filter "name=$1" | awk 'NR>1{print $1}') --details --since=$(echo $date_old) --until=$(echo $date_now)
 }
