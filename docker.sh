f643152ba45311b7055b0d2a522b504c4d9da173
Sergey <xxsmotur@gmail.com>
2020-06-07 18:17:20 +0500

    Fixing of names

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 
 # docker images отсортированный по размеру образа
-dimages_sort(){
+dimsorted(){
     docker images --format "{{.ID}}\t{{.Repository}}\t{{.Size}}" | awk '{sub(/GB/, "", $3)}1' | awk '{$3=($3 ~ /MB/) ? $3 : $3*1000"MB"}1' | sort -k 3 -h
 }
 
@@ -20,8 +20,13 @@ dvolumes() {
 # date range использует формат команды date
 # dlogs_out redis "-1 days"
 # возвращает логи, которые были в контейнеры со вчерашнего дня до текущего момента
-dlogs_out () {
+dlogsrange () {
     date_old=$(date --rfc-3339=seconds -d "$2"| sed 's/ /T/' | cut -f1 -d"+")
     date_now=$(date --rfc-3339=seconds | sed 's/ /T/' | cut -f1 -d"+")
     docker logs $(docker ps --filter "name=$1" | awk 'NR>1{print $1}') --details --since=$(echo $date_old) --until=$(echo $date_now)
+}
+
+# Колоризированный вывод json
+dinspect () {
+    docker inspect $1 | jq
 }
