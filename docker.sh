f106cd16cfabf6cb285420836cef5bb39c3f3f8e
Sergey <xxsmotur@gmail.com>
2020-06-07 18:02:12 +0500

    Add more commands

--- docker.sh
+++ docker.sh
@@ -3,3 +3,13 @@ docker images --format "{{.ID}}\t{{.Repository}}\t{{.Size}}" | awk '{sub(/GB/, "
 dexec() {
     docker exec -ti $(docker ps --filter "name=$1" | awk 'NR>1{print $1}') sh
 }
+
+dvolumes() {
+    docker volume ls --filter "driver=local" --format '{{.Driver}} {{.Name}} {{.Labels}}' | less
+}
+
+dlogs () {
+    date_old=$(date --rfc-3339=seconds -d "$1"| sed 's/ /T/' | cut -f1 -d"+")
+    date_now=$(date --rfc-3339=seconds | sed 's/ /T/' | cut -f1 -d"+")
+    docker logs 050e94357f7d --details --since=$(echo $date_old) --until=$(echo $date_now)
+}
